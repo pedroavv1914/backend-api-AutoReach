826460d27c270fd5cedd22184cdcd631df8d1e2b
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-09 20:38:18 -0300

    feat: implement initial backend structure with auth, posts, media and queue modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { MediaModule } from './media/media.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PostsModule, MediaModule, QueueModule],
   controllers: [AppController],
   providers: [AppService],
 })
