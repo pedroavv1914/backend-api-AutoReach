c71b6bb007cebde8abf57a7eae67715346c424c9
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-09 20:53:49 -0300

    feat: implement post scheduling with queue system and CRUD endpoints

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [QueueService],
   exports: [QueueService],
 })
