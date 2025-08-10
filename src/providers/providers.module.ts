5d3995223d4a77209285f8980459dd3a9d5a73df
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-10 17:42:11 -0300

    feat: implement Twitter OAuth1/2 integration with debug endpoints

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ProvidersService } from './providers.service';
 import { TwitterService } from './twitter.service';
+import { TwitterDebugController } from './twitter-debug.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [TwitterDebugController],
   providers: [ProvidersService, TwitterService],
   exports: [ProvidersService],
 })
