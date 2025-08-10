0469a82b8f5d46c2102f039d63af474a0f89ebf4
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-09 21:28:25 -0300

    feat: create OAuth and Accounts modules for social media integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { MediaModule } from './media/media.module';
 import { QueueModule } from './queue/queue.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { OAuthModule } from './oauth/oauth.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, MediaModule, QueueModule],
+  imports: [AuthModule, UsersModule, PostsModule, MediaModule, QueueModule, AccountsModule, OAuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
