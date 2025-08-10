43c8bb18ee8d9eed26952935e32bfc6efec10b3f
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-09 22:14:05 -0300

    feat: implement queue system for social media post publishing with BullMQ

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ProvidersModule } from '../providers/providers.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProvidersModule],
   providers: [QueueService],
   exports: [QueueService],
 })
