5a81c1f13b463c6ba7941994599a9be82d48ae8a
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-24 12:15:06 -0300

    feat: add OAuth authentication for social media accounts

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AccountsController],
-  providers: [AccountsService],
-  exports: [AccountsService],
+  providers: [],
 })
 export class AccountsModule {}
