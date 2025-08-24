57d5fc0cd05799222ff25c40a9b054f8ed85499e
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-24 20:00:06 -0300

    feat: add OAuth authentication for social media accounts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -8,10 +8,31 @@ import { MediaModule } from './media/media.module';
 import { QueueModule } from './queue/queue.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { ProvidersModule } from './providers/providers.module';
+import { CommonModule } from './common/common.module';
+import { TenantsModule } from './tenants/tenants.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TenantMiddleware } from './common/tenant.middleware';
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, MediaModule, QueueModule, AccountsModule, ProvidersModule],
+  imports: [
+    PrismaModule,
+    CommonModule,
+    TenantsModule,
+    AuthModule, 
+    UsersModule, 
+    PostsModule, 
+    MediaModule, 
+    QueueModule, 
+    AccountsModule, 
+    ProvidersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(TenantMiddleware)
+      .forRoutes('*'); // Apply to all routes
+  }
+}
